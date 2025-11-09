70950f477b401ce5ee2917eb3361ace7a3a66af8
AdithKumar Etikyala <adith2605@gmail.com>
2025-11-09 10:41:43 +0530

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "campusexaminer-ok-909128-22fed",
-  "appId": "1:1030018050885:web:4f852011321fea1e61ec2c",
-  "apiKey": "AIzaSyDIjOn9hw8WKidqSAPC8n7eSU2HLys8Yt0",
-  "authDomain": "campusexaminer-ok-909128-22fed.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1030018050885"
-};
--- src/lib/firebase.ts
+++
@@ -1,4 +0,0 @@
-// This file is deprecated. Imports should be made from '@/firebase' instead.
-// For backwards compatibility, we can re-export from the new central point.
-
-export * from '@/firebase';
